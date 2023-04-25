e731124817cf8822a03e5d6d1348b610eef49a82
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-25 13:09:41 -0700

    Added rule_of_72 file
    Added rule_of_72 file

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let interestRate = 12;
+let years = 72 / interestRate;
+// let worth = ;
+
+console.log("At a " + interestRate + " interest rate, your savings account will be worth " + /*worth.toFixed(2) +*/ " in " + years.toFixed(1) + " years.");
