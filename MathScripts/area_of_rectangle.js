4b88365a05a1406cdee01b9aee3c4b6389553f89
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-25 11:33:50 -0700

    First commit
    First commit

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let length = 5;
+let width = 2;
+let areaOfRectangle = length * width;
+
+console.log("The area of the rectangle is " + areaOfRectangle);
---
+++ MathScripts/net_worth.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let assets = 100000;
+let debts = 24000;
+let netWorth = assets - debts;
+
+console.log('Your net worth is ' + netWorth);
