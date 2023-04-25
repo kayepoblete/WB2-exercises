81e51f712b9c68d1c4f81f64fe9d141d4ace983d
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-25 13:19:03 -0700

    Added f_to_c and c_to_f files
    Added f_to_c and c_to_f files

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,7 @@
+"uses strict"
+
+let celsius = 32;
+let fahrenheit = (celsius * (9/5)) + 32;
+
+
+console.log(celsius + " degrees Celsius" + " is " + fahrenheit.toFixed(2) + " degrees Fahrenheit.");
---
+++ MathScripts/f_to_c.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let fahrenheit = 98;
+let celsius = (fahrenheit - 32)*(5/9);
+
+console.log(fahrenheit + " degrees Fahrenheit" + " is " + celsius.toFixed(2) + " degrees Celsius.");
