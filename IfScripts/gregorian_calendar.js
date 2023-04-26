689cfa9fc54d75b81ee264b4e9353eeaff133e3d
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-26 15:11:27 -0700

    Created gregorian_calendar.js

---
+++ IfScripts/gregorian_calendar.js
@@ -0,0 +1,10 @@
+//Determine whether a given year is a leap year in the Gregorian Calendar.
+
+let year = 2000;
+
+if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+    console.log(year + " is a leap year.")
+}
+else{
+    console.log(year + " is not a leap year.")
+}
