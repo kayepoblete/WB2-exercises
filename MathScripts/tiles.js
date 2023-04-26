e7cef2d39599e211de7985d85bd3a47e73be20b8
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-26 10:06:41 -0700

    Update tiles file
    Update tiles file

--- MathScripts/tiles.js
+++ MathScripts/tiles.js
@@ -1,8 +1,9 @@
 "use strict"
 
-let length = 60;
-let width = 72;
-let numberOfBoxes
-let totalBoxes = numberOfBoxes + (numberOfBoxes * 0.10);
+let length = 30;
+let width = 15;
+let areaOfRoom = length * width;
+let numberOfBox = (areaOfRoom + (areaOfRoom * 0.10)) / 12;
 
-console.log("You will need " + totalBoxes);
+
+console.log("You will need " + Math.ceil(numberOfBox) + " box(es).");
