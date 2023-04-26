b3268284bf8ecb14ea129094651b73a2aec34b08
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-26 15:25:32 -0700

    Created greeting.js
    Created greeting.js

---
+++ IfScripts/greeting.js
@@ -0,0 +1,15 @@
+
+let currentHour = 17;
+let greeting;
+
+if (currentHour < 10) {
+    greeting = "Good morning!";
+}
+else if (currentHour < 17) {
+    greeting = "Good day!";
+}
+else {
+    greeting = "Good evening!";
+}
+
+console.log(greeting);
