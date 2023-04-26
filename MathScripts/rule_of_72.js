5ddc01dfa4c4672d2ee338f6a228158a44fe0e3e
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-25 21:46:17 -0700

    Updated rule_of_72 file
    Updated rule_of_72 file

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -1,7 +1,8 @@
 "use strict"
 
 let interestRate = 12;
+let balance = 10000;
 let years = 72 / interestRate;
-// let worth = ;
+let worth = balance * 2;
 
-console.log("At a " + interestRate + " interest rate, your savings account will be worth " + /*worth.toFixed(2) +*/ " in " + years.toFixed(1) + " years.");
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth " + worth.toFixed(2) + " in " + years.toFixed(1) + " years.");
--- MathScripts/tiles.js
+++ MathScripts/tiles.js
@@ -3,3 +3,6 @@
 let length = 60;
 let width = 72;
 let numberOfBoxes
+let totalBoxes = numberOfBoxes + (numberOfBoxes * 0.10);
+
+console.log("You will need " + totalBoxes);
