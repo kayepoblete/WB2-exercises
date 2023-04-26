22637314c646b2b9659077d2343f6aa5dc67baea
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-26 10:58:27 -0700

    Added NumericConversions

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,32 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+// Convert using parseInt()
+console.log(parseInt(a));
+console.log(parseInt(b));
+console.log(parseInt(c));
+console.log(parseInt(d));
+
+// Convert using parseFloat()
+console.log(parseFloat(a));
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
+
+// Convert using Number()
+console.log(Number(a));
+console.log(Number(b));
+console.log(Number(c));
+console.log(Number(d));
+
+// Convert using the unary + operator
+console.log(+a)
+console.log(+b)
+console.log(+c)
+console.log(+d)
