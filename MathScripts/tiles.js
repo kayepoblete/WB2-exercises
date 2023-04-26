aec620f87d4afaf3ff4233b3754a93083d8f8c2a
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-04-26 11:47:12 -0700

    Update tiles.js

--- MathScripts/tiles.js
+++ MathScripts/tiles.js
@@ -1,9 +1,9 @@
 "use strict"
 
-let length = 30;
-let width = 15;
+let length = 10;
+let width = 20;
 let areaOfRoom = length * width;
-let numberOfBox = (areaOfRoom + (areaOfRoom * 0.10)) / 12;
+let numberOfBox = (areaOfRoom / 12) * 1.1;
 
 
 console.log("You will need " + Math.ceil(numberOfBox) + " box(es).");
